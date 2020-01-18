a6a32a1077906d8c8a45447b79d7f912733e51dd
Oxyps <abel_castro_junior@hotmail.com>
2020-01-17 23:39:58 -0300

    first commit

---
+++ src/services/population.js
@@ -0,0 +1,26 @@
+[
+    {
+        "github_username": "StheffanyHadlich",
+        "techs": "React.js, Laravel",
+        "longitude": -50.612434,
+        "latitude": -25.4570443
+    },
+    {
+        "github_username": "diego3g",
+        "techs": "React.js, Node.js, ReactNative",
+        "longitude": -50.650945899999996,
+        "latitude": -25.4700302
+    },
+    {
+        "github_username": "oxyps",
+        "techs": "Java, Spring, Javascript, React.js, Python, Django",
+        "longitude": -50.5969252,
+        "latitude": -25.4666589
+    },
+    {
+        "github_username": "milenabastosribas",
+        "techs": "React.js, Laravel",
+        "longitude": -50.6134776,
+        "latitude": -25.4583238
+	}
+]
